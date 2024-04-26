98de6947911fa45d987304f4c48a77fda7fdf820
Jashandeep Singh <jashandeep1659@gmail.com>
2024-04-26 18:53:51 +0530

    Refactor API endpoint URL in order page

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -73,142 +73,145 @@ const OrderPage = () => {
     return <div className="container md:mt-12 mt-6">Loading auth...</div>;
   }
   if (session.isAuthenticated === false) {
-    router.push("/login");
+    router.push("/api/auth/login?");
   }
-
-  return (
-    <div className="container md:mt-12 mt-6">
-      {/* <h1 className="text-lg font-bold">Order Details</h1>
+  if (session.isAuthenticated) {
+    return (
+      <div className="container md:mt-12 mt-6">
+        {/* <h1 className="text-lg font-bold">Order Details</h1>
       <p className="text-sm text-muted-foreground ">
         Enter your address details{" "}
       </p> */}
 
-      <div className="">
-        <h2 className="text-lg font-bold">Plant Quantity</h2>
-        <p className="text-sm text-muted-foreground">Price of 1 is ₹100.</p>
+        <div className="">
+          <h2 className="text-lg font-bold">Plant Quantity</h2>
+          <p className="text-sm text-muted-foreground">Price of 1 is ₹100.</p>
 
-        <div className="mt-6">
-          <h2 className="text-lg font-bold text-muted-foreground flex gap-2">
-            Quantity:{" "}
-            <span className="text-foreground">{String(Quantity)}</span> at
-            <span className="text-foreground">₹{Quantity * 100}</span>
-          </h2>
+          <div className="mt-6">
+            <h2 className="text-lg font-bold text-muted-foreground flex gap-2">
+              Quantity:{" "}
+              <span className="text-foreground">{String(Quantity)}</span> at
+              <span className="text-foreground">₹{Quantity * 100}</span>
+            </h2>
 
-          <div className="flex flex-wrap gap-2 items-center mt-4">
-            <button
-              onClick={() => {
-                Quantity > 1 && setQuantity(Quantity - 1);
-              }}
-              className="minus border border-primary rounded-full p-1 hover:bg-primary text-primary hover:text-white duration-300"
-            >
-              <Minus size={16} />
-            </button>
-            <span className="px-3">{String(Quantity)}</span>
-            <button
-              onClick={() => {
-                setQuantity(Quantity + 1);
-              }}
-              className="plus border border-primary rounded-full p-1 hover:bg-primary text-primary hover:text-white duration-300"
-            >
-              <Plus size={16} />
-            </button>
+            <div className="flex flex-wrap gap-2 items-center mt-4">
+              <button
+                onClick={() => {
+                  Quantity > 1 && setQuantity(Quantity - 1);
+                }}
+                className="minus border border-primary rounded-full p-1 hover:bg-primary text-primary hover:text-white duration-300"
+              >
+                <Minus size={16} />
+              </button>
+              <span className="px-3">{String(Quantity)}</span>
+              <button
+                onClick={() => {
+                  setQuantity(Quantity + 1);
+                }}
+                className="plus border border-primary rounded-full p-1 hover:bg-primary text-primary hover:text-white duration-300"
+              >
+                <Plus size={16} />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <hr className="my-8" />
+        <div>
+          <h2 className="text-lg font-bold ">Address</h2>
+          <p className="text-sm text-muted-foreground">
+            Your address where you want to deliver these plants.
+          </p>
+          <div className="">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className=" grid gap-8 md:grid-cols-2 mt-6"
+              >
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Full Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Jashandeep Singh" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        Your full name as per your ID
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="phoneNumber"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Phone Number</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="number"
+                          placeholder="99123-99123"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        Without including the country code.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="address"
+                  render={({ field }) => (
+                    <FormItem className="md:col-span-2">
+                      <FormLabel>Address</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder="Tell us a little about your address where you want to ship the plants"
+                          className="resize-none"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>Your complete address</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="instruction"
+                  render={({ field }) => (
+                    <FormItem className="md:col-span-2">
+                      <FormLabel>Instruction</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="text"
+                          placeholder="Don't ship on sunday"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        Any instructions that you want, we would like to follow.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Proceed to Pay</Button>
+              </form>
+            </Form>
           </div>
         </div>
       </div>
-
-      <hr className="my-8" />
-      <div>
-        <h2 className="text-lg font-bold ">Address</h2>
-        <p className="text-sm text-muted-foreground">
-          Your address where you want to deliver these plants.
-        </p>
-        <div className="">
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className=" grid gap-8 md:grid-cols-2 mt-6"
-            >
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Full Name</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Jashandeep Singh" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      Your full name as per your ID
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="phoneNumber"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Phone Number</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="number"
-                        placeholder="99123-99123"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormDescription>
-                      Without including the country code.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="address"
-                render={({ field }) => (
-                  <FormItem className="md:col-span-2">
-                    <FormLabel>Address</FormLabel>
-                    <FormControl>
-                      <Textarea
-                        placeholder="Tell us a little about your address where you want to ship the plants"
-                        className="resize-none"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormDescription>Your complete address</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="instruction"
-                render={({ field }) => (
-                  <FormItem className="md:col-span-2">
-                    <FormLabel>Instruction</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="text"
-                        placeholder="Don't ship on sunday"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormDescription>
-                      Any instructions that you want, we would like to follow.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Proceed to Pay</Button>
-            </form>
-          </Form>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return <div>loading</div>;
+  }
 };
 
 export default OrderPage;
